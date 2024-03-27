2a1459b04633116dc5b6d4075424bb322b1f015b
Getaneh48 <getaneh11@gmail.com>
2024-03-27 08:32:26 +0300

    betty error fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - a function that creates a binary tree
  * @parent: Pointer to the parrent node
  * @value: integer to be stored to the node
- * @Returns: a tree node
+ * Return: a tree node
 */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
