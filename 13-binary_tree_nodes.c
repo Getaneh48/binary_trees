0c1b1c33561f08617dabeedc31740d5fdc99a226
Getaneh48 <getaneh11@gmail.com>
2024-03-29 14:39:37 +0300

    added return 0 if the tree is null

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 	size_t nodes = 0;
 
 	if (tree == NULL)
-		return (NULL);
+		return (0);
 
 	nodes += (tree->left || tree->right) ? 1 : 0;
 	nodes += binary_tree_nodes(tree->left);
