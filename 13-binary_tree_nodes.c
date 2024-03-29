735043ced2165a8f58af71a357d13c208b1c9104
Getaneh48 <getaneh11@gmail.com>
2024-03-29 14:29:42 +0300

    added a check if a tree is null

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,6 +18,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 		nodes += binary_tree_nodes(tree->left);
 		nodes += binary_tree_nodes(tree->right);
 	}
+	else
+		return (NULL);
 
 	return (nodes);
 }
